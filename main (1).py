75011f206ab663892e7d40d5d1f9d2aca58bf528
emanuellefurmanovicz <128699187+emanuellefurmanovicz@users.noreply.github.com>
2024-02-15 10:46:44 -0300

    Add files via upload

---
+++ main (1).py
@@ -0,0 +1,17 @@
+#nome = input("escreva seu nome")
+#try: 
+    #idade = int(input("escreva sua idade"))
+#except ValueError:
+    
+    #print("voce nao digitou um numero")
+
+#print (f"ola,{nome},voce tem {idade} anos.")
+
+numero1 = float(input("digite um numero"))
+numero2 = float(input("digite  mais um numero"))
+
+
+print(f" o numero{n1} mais o numero {n2} e igual a: soma = n1+ n2")
+
+soma = n1 + n2 
+
