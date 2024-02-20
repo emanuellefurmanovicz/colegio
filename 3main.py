0aa5ba58668a93749a27b21e4e65493be5df64ec
emanuellefurmanovicz <128699187+emanuellefurmanovicz@users.noreply.github.com>
2024-02-20 10:46:39 -0300

    Add files via upload

---
+++ 3main.py
@@ -0,0 +1,44 @@
+#nome = input("escreva seu nome")
+#try: 
+    #idade = int(input("escreva sua idade"))
+#except ValueError:
+
+    #print("voce nao digitou um numero")
+
+#print (f"ola,{nome},voce tem {idade} anos.")
+#try:
+    
+    #n1 = int(input("digite um numero:"))
+    #n2 = int(input("digite  mais um numero:"))
+#except ValueError:
+    #print("tente novamente")
+#soma = n1 + n2
+
+
+#print(f" o numero{n1} mais o numero {n2} e igual a: {soma}")
+
+#soma = n1 + n2 
+
+
+
+# Solicita a idade do usuário
+nota = int(input("Digite sua nota: "))
+# Categoriza a idade
+if nota >= 90:
+    categoria = "A"
+elif  nota >= 80:
+    categoria = "B"
+elif  nota >= 70:
+    categoria = "C"
+elif nota >= 60:
+    categoria = "D"
+else:
+    categoria = "F"
+# Exibe a categoria
+print(f"Você está na categoria: {categoria}.")
+
+
+
+
+
+
