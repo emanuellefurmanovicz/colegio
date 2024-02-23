711ab073bf0219ad9cd64588aafe28cb88fb6918
emanuellefurmanovicz <128699187+emanuellefurmanovicz@users.noreply.github.com>
2024-02-23 11:51:34 -0300

    Add files via upload

---
+++ nome_idade.js
@@ -0,0 +1,7 @@
+let nome = "manu";
+let idade = 17;
+let inteiro = 4;
+let altura = 1.70;
+let peso = 50;
+console.log(`meu nome e ${nome} e minha idade e ${idade}`);
+console.log(`a iade e ${idae}`)
---
+++ string.js
@@ -0,0 +1,12 @@
+let nome = 'manu';
+let endereco = 'marechal rondon';
+let cachorro1 = 'perola';
+let cachorro2 = 'pink';
+let cachorro3 = 'neguinha';
+console.log (`o nome do cachorro1 e ${cachorro1}`);
+console.log (`o nome do cachorro2 e ${cachorro2}`);
+console.log (`o nome do cachorro3 e ${cachorro3}`);
+console.log (`o nome da aluna e ${nome}`);
+console.log (`o endereço da aluna e ${endereco}`);    
+
+
