79f4e45c65d4049d5ce36199117b47896b90d166
emanuellefurmanovicz <128699187+emanuellefurmanovicz@users.noreply.github.com>
2024-02-26 11:38:29 -0300

    Add files via upload

---
+++ obj.js
@@ -0,0 +1,3 @@
+let idade = [17,16,1,18,18]
+let nome = ["manu","ju","pedro","igor","fuzi"]
+console.log(`o ${nome[3]} tem ${idade[3]}`)
---
+++ soma.js
@@ -0,0 +1,4 @@
+let soma = 5 + 3;
+let diferenca = 5 - 3;
+let multiplicacao = 
+console.log(`a soma de 5+3 e `)
