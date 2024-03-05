bcf8d0f1c344b0de9b2972e5fc3baf59a17f1e37
emanuellefurmanovicz <128699187+emanuellefurmanovicz@users.noreply.github.com>
2024-03-05 08:27:24 -0300

    Create jogos.py

---
+++ jogos.py
@@ -0,0 +1,13 @@
+import forca.py;
+import adivinhacao.py;
+def escolha_jogos():
+    print('********')
+    print('*Escolha o jogo*')
+    print('********')
+    print('(1)Forca (2)Adivinhação')
+    jogo = print int(input("Qual jogo?"))
+    if (jogo == 1)
+    print('Jogo da Forca')
+    forca.jogar()
+    elif(jogo == 2):
+        print("Jogando Adivinhação")
